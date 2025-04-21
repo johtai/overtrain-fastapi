82d281e1950b86d79016e66544c175c2f83dd41b
Романченко Надежда Романиевна <peterwaybright@gmail.com>
2025-04-21 12:40:15 +0300

    add basic routes

--- main.py
+++ main.py
@@ -2,8 +2,28 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
  
 app = FastAPI()
- 
+
+@app.get("/error")
+def error():
+    html_content = "<h2>ERROR page</h2>"
+    return HTMLResponse(content=html_content)
+
+@app.get("/search")
+def search():
+    html_content = "<h2>Search page</h2>"
+    return HTMLResponse(content=html_content)
+
+@app.get("/help")
+def help():
+    html_content = "<h2>Help page</h2>"
+    return HTMLResponse(content=html_content)
+
+@app.get("/about")
+def about():
+    html_content = "<h2>About page</h2>"
+    return HTMLResponse(content=html_content)
+
 @app.get("/")
-def read_root():
-    html_content = "<h2>Overtrain!</h2>"
+def root():
+    html_content = "<h2>Overtrain! init 2</h2>"
     return HTMLResponse(content=html_content)
